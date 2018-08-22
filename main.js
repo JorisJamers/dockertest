e3f506121d53abeec6d02e54a16b8a7dddabef56
JorisJamers <jorisjamers22@hotmail.com>
2018-08-22 10:49:39 +0200

    test

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("The site text has changed\n");
+  response.end("The thing has changed\n");
 });
 
 // listen on localhost:8000
