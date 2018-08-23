0cf6c4c7c8816d529602c462273a0ba13f281aa9
JorisJamers <jorisjamers22@hotmail.com>
2018-08-23 11:36:59 +0200

    test

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Werkt mijn multibranch?\n");
+  response.end("Nieuwe jenkins, still works!\n");
 });
 
 // listen on localhost:8000
