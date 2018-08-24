c51bb2310398ad7e4f6dbae0b8ff6572c63f0898
JorisJamers <jorisjamers22@hotmail.com>
2018-08-24 14:17:31 +0200

    work!

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("hello?\n");
+  response.end("c'mon jenkins werke nondeju!?\n");
 });
 
 // listen on localhost:8000
