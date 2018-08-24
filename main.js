3dbe66400bee53ac16795a6e715a7f450fa4e863
JorisJamers <jorisjamers22@hotmail.com>
2018-08-24 11:34:22 +0200

    update

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("If this gets online, the pipeline works!!\n");
+  response.end("The rolling update\n");
 });
 
 // listen on localhost:8000
