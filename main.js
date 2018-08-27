dc26c544df560089753a31c80712657864c11ea1
JorisJamers <jorisjamers22@hotmail.com>
2018-08-27 10:53:15 +0200

    does the pipeline work?

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("update!\n");
+  response.end("With this line of code It might all work!!\n");
 });
 
 // listen on localhost:8000
