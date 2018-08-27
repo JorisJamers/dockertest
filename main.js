934c3c6006e6a42e13bb9e07cf39a11712381304
JorisJamers <jorisjamers22@hotmail.com>
2018-08-27 14:40:00 +0200

    test

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Newer version of the website.\n");
+  response.end("Welcome.\n");
 });
 
 // listen on localhost:8000
