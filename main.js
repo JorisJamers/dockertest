ed78504a9ba80210209f5a466d11cef4b6ea156d
JorisJamers <jorisjamers22@hotmail.com>
2018-08-27 11:30:38 +0200

    update

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("This is the version after the rerun of the ansible playbook.\n");
+  response.end("This is the updated version of the website.\n");
 });
 
 // listen on localhost:8000
