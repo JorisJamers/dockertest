2431e4a89ac65d6cc98c9cecf52f953dd34eacab
JorisJamers <jorisjamers22@hotmail.com>
2018-08-29 11:08:43 +0200

    test

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("New seddin'\n");
+  response.end("This is with the new dns records.\n");
 });
 
 // listen on localhost:8000
