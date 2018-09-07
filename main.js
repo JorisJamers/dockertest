474e26bfc2871fd4a82b355676833fa324ed57cf
JorisJamers <jorisjamers22@hotmail.com>
2018-09-07 07:57:08 +0200

    Should still work?

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello, world! How are you?\n");
+  response.end("So, this should still work, does it?\n");
 });
 
 // listen on localhost:8000
